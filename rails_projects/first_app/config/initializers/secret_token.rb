67a3fe101df1291c19e62ae4668f4cdb79cfe1cd
Alexander <alex.egorov@outlook.com>
2013-03-17 12:23:07 +0100

    new interation with heroku

--- rails_projects/first_app/config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-FirstApp::Application.config.secret_token = '1dfbe614501866f6f3bb3210f9c3f829c73b99dc353c8e3d1d6292719833a42a5e630b13a4bb1b6efbd91a57ddd7cb53463664b9648fea0450bc39a44ceb6dea'
